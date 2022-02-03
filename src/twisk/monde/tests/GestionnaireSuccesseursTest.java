e0b2975e689aa3b473cd06364c1842bcc45c9d0e
suel1u <@suel1u>
2022-02-03 11:21:03 +0100

    Ajout de la classe de test de GestionnaireSuccesseur

--- src/twisk/monde/tests/GestionnaireSuccesseursTest.java
+++ src/twisk/monde/tests/GestionnaireSuccesseursTest.java
@@ -4,40 +4,28 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import twisk.monde.*;
 
-import java.util.Iterator;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GestionnaireSuccesseursTest {
 
     private GestionnaireSuccesseurs gestS;
-    private GestionnaireSuccesseurs gestEmpty;
 
     @BeforeEach
     void setUp() {
         this.gestS = new GestionnaireSuccesseurs();
-        this.gestEmpty =new GestionnaireSuccesseurs();
     }
 
     @Test
     void ajouter() {
         Etape E1 = new Activite("Etape1");
         Etape G1 = new Guichet("Guichet1");
+
         this.gestS.ajouter(E1,G1);
+
         assertEquals(2,this.gestS.nbEtapes());
     }
 
     @Test
     void iterator() {
-        Etape act = new Activite("Toboggan");
-        Etape guich = new Guichet("Caisse");
-        this.gestS.ajouter(act,guich);
-        Iterator<Etape> it, it2;
-        it = this.gestEmpty.iterator();
-        it2 = this.gestS.iterator();
-        assertFalse(it.hasNext());
-        assertEquals(it2.next().getNom(),"Toboggan");
-        assertEquals(it2.next().getNom(),"Caisse");
-        assertFalse(it2.hasNext());
     }
 }
