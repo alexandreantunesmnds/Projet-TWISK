d538ec4b1359ac9f18619c5d8c1b4e7fdd41e7e3
suel1u <@suel1u>
2022-02-03 12:16:51 +0100

    Ajout des commentaires de la classe Activité restreinte

--- src/twisk/monde/ActiviteRestreinte.java
+++ src/twisk/monde/ActiviteRestreinte.java
@@ -1,14 +1,28 @@
 package twisk.monde;
 
 public class ActiviteRestreinte extends Activite{
+    /**
+     * Constructeur
+     * @param nom nom de l'activité restreinte
+     */
     public ActiviteRestreinte(String nom) {
         super(nom);
     }
 
+    /**
+     * Constructeur
+     * @param nom nom de l'activité restreinte
+     * @param t temps de l'activité restreinte
+     * @param e l'écart-temps de l'activité restreinte
+     */
     public ActiviteRestreinte(String nom, int t, int e) {
         super(nom, t, e);
     }
 
+    /**
+     * Fonction toString
+     * @return retourne les détails de l'activité restreinte
+     */
     @Override
     public String toString() {
         return "ActiviteRestreinte{}";
