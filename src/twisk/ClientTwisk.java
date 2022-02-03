1835e7a83b869b169b9e6117815775724b1f2605
Benjamin <bensuel54@gmail.com>
2022-02-03 16:20:07 +0100

    Réorganisation des packages et classes selon les consignes

--- src/twisk/ClientTwisk.java
+++ src/twisk/ClientTwisk.java
@@ -10,7 +10,7 @@ public class ClientTwisk {
 
     public static void main(String[] args) {
         Monde world = new Monde();
-        testApresMidi(world);
+        testPompeAEssence(world);
         Simulation play = new Simulation();
         play.simuler(world);
     }
@@ -32,37 +32,5 @@ public class ClientTwisk {
         stationEssence.aCommeSortie(pompeAEssence,caisse,gonflementPneu);
         stationEssence.ajouter(attentePompe,pompeAEssence,attenteCaisse,caisse,attentePneu,gonflementPneu);
 
-    }
-
-    public static void testRestaurant(Monde restauration){
-        Etape attentePass = new Guichet("AttentePassVaccinale", 1);
-        Etape montrerPass = new Activite("MontrerPassVaccinale", 1,2);
-        Etape attenteBorne = new Guichet("AttenteBorneDeCommande", 5);
-        Etape commander = new Activite("Commander", 10,2);
-        Etape attendreCommande = new Activite("AttendreCommande",30,2);
-        Etape mangerSurPlace = new Activite("MangerSurPlace", 120,2);
-
-        attentePass.ajouterSuccesseur(mangerSurPlace);
-        montrerPass.ajouterSuccesseur(attenteBorne);
-        attenteBorne.ajouterSuccesseur(commander);
-        commander.ajouterSuccesseur(attendreCommande);
-        attendreCommande.ajouterSuccesseur(mangerSurPlace);
-
-        restauration.aCommeEntree(attentePass);
-        restauration.aCommeSortie(attendreCommande,mangerSurPlace);
-        restauration.ajouter(attentePass,montrerPass,attenteBorne,commander,attendreCommande,mangerSurPlace);
-    }
-
-    public static void testApresMidi(Monde aprem){
-        Etape baladeAuZoo = new Activite("Ballade au zoo");
-        Etape attenteToboggan = new Guichet("AttenteToboggan",2);
-        Etape toboggan = new Activite("Toboggan",2,1);
-
-        baladeAuZoo.ajouterSuccesseur(attenteToboggan);
-        attenteToboggan.ajouterSuccesseur(toboggan);
-
-        aprem.aCommeEntree(baladeAuZoo);
-        aprem.aCommeSortie(toboggan);
-        aprem.ajouter(baladeAuZoo,attenteToboggan,toboggan);
     }
 }
