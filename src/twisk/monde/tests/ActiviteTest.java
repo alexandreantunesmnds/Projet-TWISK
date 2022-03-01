ff094eb98ca3bc44575654915d77fb361a23a230
antunes26u <alexandre.antunes4@etu.univ-lorraine.fr>
2022-03-01 15:23:14 +0100

    Supression du successeur dans le test

--- src/twisk/monde/tests/ActiviteTest.java
+++ src/twisk/monde/tests/ActiviteTest.java
@@ -32,11 +32,7 @@ class ActiviteTest extends EtapeTest{
 
     @Test
     void toC(){
-        Activite actS = new Activite("Toboggan",3,2);
-        act.ajouterSuccesseur(actS);
-        String actToC = "delai(6,3);\ntransfert(Piscine,Toboggan);\n" +
-                "delai(3,2);\n";
-        String res = act.toC();
-        assertEquals(res,actToC);
+        String actToC = "delai(6,3);\n";
+        assertEquals(act.toC(),actToC);
     }
 }
