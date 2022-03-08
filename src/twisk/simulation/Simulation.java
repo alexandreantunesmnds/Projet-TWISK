ddece6cada4bc6ee8a3df3586b2f389122595524
Benjamin <bensuel54@gmail.com>
2022-03-08 18:27:33 +0100

    Ajout de la fonction creerEnvironnement dans Simulation

--- src/twisk/simulation/Simulation.java
+++ src/twisk/simulation/Simulation.java
@@ -1,10 +1,12 @@
 package twisk.simulation;
 
 import twisk.monde.Monde;
+import twisk.outils.KitC;
 
 public class Simulation {
     public Simulation(){
-
+        KitC kc = new KitC();
+        kc.creerEnvironnement();
     }
 
     public void simuler(Monde monde){
