b3d08bd62c0428c46035cbc7473cb84b07ca10d4
Benjamin <bensuel54@gmail.com>
2022-03-08 16:24:30 +0100

    Ecriture de la classe KitC (à tester)

---
+++ src/twisk/outils/tests/KitCTest.java
@@ -0,0 +1,15 @@
+package twisk.outils.tests;
+
+import org.junit.jupiter.api.Test;
+import twisk.outils.KitC;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KitCTest {
+
+    @Test
+    void creerEnvironnement() {
+        KitC kc = new KitC();
+        //assertThrows(IOException,kc.creerEnvironnement());
+    }
+}
