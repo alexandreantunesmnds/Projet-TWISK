96cb817a47f069e9f6d6acb48223493648c8b87e
Benjamin <bensuel54@gmail.com>
2022-03-08 18:25:39 +0100

    Test de la fonction creerEnvironnement

--- src/twisk/outils/tests/KitCTest.java
+++ src/twisk/outils/tests/KitCTest.java
@@ -3,6 +3,8 @@ package twisk.outils.tests;
 import org.junit.jupiter.api.Test;
 import twisk.outils.KitC;
 
+import java.io.File;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class KitCTest {
@@ -10,6 +12,13 @@ class KitCTest {
     @Test
     void creerEnvironnement() {
         KitC kc = new KitC();
-        //assertThrows(IOException,kc.creerEnvironnement());
+        kc.creerEnvironnement();
+        File dos = new File("/tmp/twisk");
+        File prog = new File("/tmp/twisk/programmeC.o");
+        File def = new File("/tmp/twisk/def.h");
+
+        assertTrue(dos.exists() && dos.isDirectory()); //Teste si le chemin est bien un dossier et qu'il existe
+        assertTrue(prog.exists() && !prog.isDirectory()); //Teste si le chemin est bien un fichier et qu'il existe
+        assertTrue(def.exists() && !def.isDirectory()); //Teste si le chemin est bien un fichier et qu'il existe
     }
 }
